83a9570b172e80d99c8c72c91c707c71dc50852e
Jongwoo Lee <jongwoo@jongwoo.dev>
2026-09-12 22:20:10 +0900

    Initial Commit

---
+++ OuterView/OuterViewApp.swift
@@ -0,0 +1,32 @@
+//
+//  OuterViewApp.swift
+//  OuterView
+//
+//  Created by 이종우 on 9/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OuterViewApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
